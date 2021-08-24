d5ef78e2f380245d49debe91a99fe6598d3da893
jsalaiz <jonathan.salaiz@gmail.com>
2021-08-23 21:49:39 -0400

    Completed the exercises

--- Method Exercises/Method Exercises/Program.cs
+++ Method Exercises/Method Exercises/Program.cs
@@ -6,7 +6,58 @@ namespace Method_Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exercise1();
+            //Exercise2();
         }
+        static void Exercise1()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"{name} wouldn't it be crazy if a {color} {animal} played in {band}?");
+        }
+        static void Exercise2()
+        {
+            Console.WriteLine("Enter the number of digits you have");
+            int numOfInts = Convert.ToInt32(Console.ReadLine());
+            int[] listOfNums = new int[numOfInts];
+            for (int i = 0; i < numOfInts; i++)
+                {
+                    Console.WriteLine("Type an integer");
+                    listOfNums[i] = Convert.ToInt32(Console.ReadLine());    
+                }
+            int sumResult = 0;
+            int multiplyResult = 0;
+            sumResult = Sum(listOfNums);
+            multiplyResult = Multiply(listOfNums);
+            Console.WriteLine($"Your integers added together are: {sumResult}");
+            Console.WriteLine($"Your integers multiplied together are: {multiplyResult}");
+
+        }
+        static int Sum(params int[] nums)
+        {
+            int result = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                result += nums[i];
+            }
+            return result;
+        }
+        static int Multiply(params int[] nums)
+        {
+            int result = 1;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                result *= nums[i];
+            }
+            return result;
+        }
+
     }
 }
